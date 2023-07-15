880f4e619ae6dda4618340f59bd6363044ee65fb
Daniel Mesquita <danielmesquitta123@gmail.com>
2023-07-15 13:26:22 -0300

    feat: omitted fields from json

--- model/user.go
+++ model/user.go
@@ -18,8 +18,8 @@ type User struct {
 	ID        uint           `json:"id,omitempty" gorm:"primarykey"`
 	CreatedAt time.Time      `json:"createdAt,omitempty"`
 	UpdatedAt time.Time      `json:"updatedAt,omitempty"`
-	DeletedAt gorm.DeletedAt `json:"deletedAt,omitempty" gorm:"index"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
 	Name      string         `json:"name,omitempty"`
 	Email     string         `json:"email,omitempty" gorm:"unique"`
-	Password  string         `json:"password,omitempty"`
+	Password  string         `json:"-"`
 }
